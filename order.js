9515538c67f0c2c24ad0a49173319256ea8f8446
NDMHVLC <nedimhavlaci@gmail.com>
2023-11-28 23:14:54 +0300

    first commit

---
+++ order.js
@@ -0,0 +1,20 @@
+const Orders = [
+    {
+        productName: 'JavaScript Tutorial',
+        productNumber: '44512',
+        paymentStatus: 'Due',
+        status: 'Pending'
+    },
+    {
+        productName: 'CSS Full Cuorse',
+        productNumber: '33150',
+        paymentStatus: 'Refundend',
+        status: 'Declined'
+    },
+    {
+        productName: 'Flex-Box Tutorial',
+        productNumber: '26554',
+        paymentStatus: 'Paid',
+        status: 'Active'
+    }
+]
